065a2b7a0c59ea535cf30a40a808446b3db11e25
Wassim Benhallam <wbenhal@amazon.com>
2023-08-11 13:19:29 -0500

    Revert "Migrate auth category"
    This reverts commit 2abc74797378466904231e776e8ce43c7de61e94.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,27 +1,27 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "awsAssistantBackend": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string"
+    "auth": {
+        "awsassistantfcd2b45d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "awsAssistantBackend": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "assistantFulfillment": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
     }
-  },
-  "auth": {
-    "awsassistantfcd2b45d": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
-  "function": {
-    "assistantFulfillment": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  }
 }
